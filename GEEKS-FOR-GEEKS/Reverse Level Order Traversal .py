759925256178dbaad790223c3e8f281b1df7509c
Yogeshwaran-D <yogesh94487@gmail.com>
2024-05-07 23:47:03 +0530

    Create : Reverse Level Order Traversal .py

---
+++ GEEKS-FOR-GEEKS/Reverse Level Order Traversal .py
@@ -0,0 +1,18 @@
+def reverseLevelOrder(root):
+    q = deque()
+    q.append(root)
+    ans = deque()
+    while q:
+        node = q.popleft()
+        if node is None:
+            continue
+
+        ans.appendleft(node.data)
+
+        if node.right:
+            q.append(node.right)
+
+        if node.left:
+            q.append(node.left)
+
+    return ans
