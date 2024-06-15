0d9ee162ec60f18b1795b3a0689c2dc00ac89df4
Yogeshwaran-D <yogesh94487@gmail.com>
2024-06-15 21:56:50 +0530

    Create : IPO .py

---
+++ LEET-CODE/502. IPO .py
@@ -0,0 +1,16 @@
+import heapq
+
+
+class Solution:
+    def findMaximizedCapital(self, k, w, profits, capital) :
+        maxProfit=[]
+        minCapital=[(c,p) for c,p in zip(capital,profits)]
+        heapq.heapify(minCapital)
+        for i in range(k):
+            while minCapital and minCapital[0][0] <=w :
+                c,p=heapq.heappop(minCapital)
+                heapq.heappush(maxProfit,-1*p)
+            if not maxProfit:
+                break
+            w+= -1 * heapq.heappop(maxProfit)
+        return w
