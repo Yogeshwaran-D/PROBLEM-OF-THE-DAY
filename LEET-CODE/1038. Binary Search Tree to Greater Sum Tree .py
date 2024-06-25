73fc30abceb2b423bfd61f1c8606612b2a374202
Yogeshwaran-D <yogesh94487@gmail.com>
2024-06-25 08:37:26 +0530

    Create :1038. Binary Search Tree to Greater Sum Tree .py

---
+++ LEET-CODE/1038. Binary Search Tree to Greater Sum Tree .py
@@ -0,0 +1,9 @@
+class Solution:
+    sum=0
+    def bstToGst(self, root) :
+        if root:
+            self.bstToGst(root.right)
+            self.sum+=root.val
+            root.val=self.sum
+            self.bstToGst(root.left)
+        return root
