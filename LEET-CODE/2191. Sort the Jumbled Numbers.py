1c667d43737b60d83404f48a3e28075c84824004
Yogeshwaran-D <yogesh94487@gmail.com>
2024-07-24 08:25:37 +0530

    Create:2191. Sort the Jumbled Numbers.py

---
+++ LEET-CODE/2191. Sort the Jumbled Numbers.py
@@ -0,0 +1,17 @@
+class Solution:
+    def sortJumbled(self, mapping: List[int], nums: List[int]) -> List[int]:
+        mapped_dict={}
+        res=[]
+        for num in nums:
+            str_num=str(num)
+            temp=0
+            for ele in str_num:
+                temp*=10
+                temp+=mapping[int(ele)]
+            if mapped_dict.get(temp):
+                mapped_dict[temp].append(num)
+            else:
+                mapped_dict[temp]=[num]
+        for i in sorted(list(mapped_dict.keys())):
+            res+=mapped_dict.get(i)
+        return res
